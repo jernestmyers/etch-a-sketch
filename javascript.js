802b332174fe0feb1e3579135b050aaefdb3e093
jernestmyers <inernestmusic@gmail.com>
2021-04-10 16:59:01 -0500

    createGrid takes argument rows to determine the number of grids per row and column. CSS grid aligns them properly. currently have a mouseover event listener on the grids to change them to white.

--- javascript.js
+++ javascript.js
@@ -1,34 +1,32 @@
 
 const gridContainer = document.querySelector(`.grid-container`);
 const gridContainerWidth = gridContainer.clientWidth
-// const gridsDimensions = ``;
 
-function createGrid(rows, columns) {
-    // const gridsDimensions = (gridContainerWidth / rows);
-    // const gridContainer = document.querySelector(`.grid-container`);
+function createGrid(rows) {
+    const gridsDimensions = (gridContainerWidth / rows);
 
-    for (let i = 0; i < rows; i++) {
-        const gridsRows = document.createElement(`div`);
-        gridsRows.classList.add('grids-rows');
-        gridContainer.appendChild(gridsRows);
-        
-        for (let j = 1; j < columns; j++) {
-            const gridsColumns = document.createElement(`div`);
-            gridsColumns.classList.add('grids-columns');
-            gridContainer.appendChild(gridsColumns);
-        }
+    for (let i = 0; i < rows*rows; i++) {
+        const grids = document.createElement(`div`);
+        grids.classList.add('grids');
+        gridContainer.appendChild(grids);
+        grids.setAttribute('style', `width: ${gridsDimensions}px; height: ${gridsDimensions}px`);
+        changeColors(grids);
     }
-
     gridTemplate(rows);
 }
 
 function gridTemplate(rows) {
     let frs = ``;
-    const gridsDimensions = (gridContainerWidth / rows);
     for (i = 0; i < rows; i++) {
         frs += `1fr `;
     }
-    gridContainer.setAttribute('style', `grid-template-rows: ${frs}; grid-template-columns: ${frs}; width: ${gridsDimensions}px; height: ${gridsDimensions}px`);
-    // gridContainer.setAttribute(`style`, `width: ${gridsDimensions}px; height: ${gridsDimensions}px`);
+    gridContainer.setAttribute('style', `grid-template-rows: ${frs}; grid-template-columns: ${frs}`);
 }
+
+function changeColors(grids) {
+    grids.addEventListener(`mouseover`, function(event) {
+        grids.setAttribute(`style`, `background-color: white`);
+    })
+}
+
 
