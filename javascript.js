3d5630c78c5e5d51d1663fa20e105dba18d544ec
jernestmyers <inernestmusic@gmail.com>
2021-04-11 13:49:33 -0500

    pausing on trying to get sketch to begin on mousedown and stop on mouseup. instead only using mouseover to sketch. added a slider from 5 to 50 for user to select a new grid size. added a button to execute change in grid size that first removes the existing grid. also added a button to reset the grid to the current user grid size. the slider is defaulted on a 16x16 grid whiich matches the default grid on page load. lastly, added code to display the selected grid size every time user changes the slider value.

--- javascript.js
+++ javascript.js
@@ -2,18 +2,8 @@
 const gridContainer = document.querySelector(`.grid-container`);
 const gridContainerWidth = gridContainer.clientWidth
 
-function createGrid(rows) {
-    const gridsDimensions = (gridContainerWidth / rows);
-
-    for (let i = 0; i < rows*rows; i++) {
-        const grids = document.createElement(`div`);
-        grids.classList.add('grids');
-        gridContainer.appendChild(grids);
-        grids.setAttribute('style', `width: ${gridsDimensions}px; height: ${gridsDimensions}px`);
-    }
-    gridTemplate(rows);
-}
-
+// creates a string of '1fr' based on number of rows
+// adds string to grid-template to determine CSS grid config
 function gridTemplate(rows) {
     let frs = ``;
     for (i = 0; i < rows; i++) {
@@ -22,51 +12,122 @@ function gridTemplate(rows) {
     gridContainer.setAttribute('style', `grid-template-rows: ${frs}; grid-template-columns: ${frs}`);
 }
 
+// creates the grid according to number of rows
+// maintains the container by distributing row spacing evenly
+// attaches a listener to each created grid
+function createGrid(rows) {
+    const gridsDimensions = (gridContainerWidth / rows);
+
+    for (let i = 0; i < rows*rows; i++) {
+        const grids = document.createElement(`div`);
+        grids.classList.add('grids');
+        gridContainer.appendChild(grids);
+        grids.setAttribute('style', `width: ${gridsDimensions}px; height: ${gridsDimensions}px`);
+        grids.addEventListener(`mouseover`, function(event) {
+            grids.setAttribute(`style`, `background-color: black`);
+            });
+        // changeColors(isDrawing);
+        // window.addEventListener(`mouseup`, function(event) {
+        //     if (isDrawing === true) {
+        //         isDrawing = false;
+        //         changeColors(grids, isDrawing);
+        //         console.log(`up`);
+        //     }
+        // });
+    }
+    gridTemplate(rows);
+}
+
+// removes all grids from the container
+function resetGrid() {
+    function removeGrid(parent) {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
+    }
+    removeGrid(gridContainer);
+}
+
+// creates a default 16 by 16 grid on page load
+createGrid(16);
+
+// takes value from slider, calls resetGrid to remove existing grid
+// then creates a new grid with the slider value
+const gridSlider = document.querySelector(`#rowsUserChoice`);
+const confirmGridChange = document.querySelector(`#confirmUserGridSize`);
+confirmGridChange.addEventListener('click', () => {
+    resetGrid();
+    createGrid(gridSlider.value);
+})
+
+// clears the grid and reloads the currently-selected grid size
+const confirmClear = document.querySelector(`#clearGrid`);
+confirmClear.addEventListener('click', () => {
+    resetGrid();
+    createGrid(gridSlider.value);
+})
+
+const inputDiv = document.querySelector(`.user-inputs`);
+const numbersOfRowsSelected = document.createElement('p');
+gridSlider.addEventListener('change', () => {
+    numbersOfRowsSelected.textContent = `${gridSlider.value} x ${gridSlider.value} grid`;
+    inputDiv.appendChild(numbersOfRowsSelected);
+})
 
 // let isDrawing = false;
-// function changeColors(grids) {
+// function changeColors(grids, condition) {
 //     gridContainer.addEventListener(`mousedown`, function(event) {
 //         isDrawing = true;
-//         window.addEventListener(`mouseup`, function(event) {
-//             isDrawing = false;
-//         });
 //         if (isDrawing === true) {
 //             grids.addEventListener(`mouseover`, function(event) {
 //             grids.setAttribute(`style`, `background-color: black`);
-//         });
-//         } else if (isDrawing === false) {
-//             grids.removeEventListener(`mouseover`, function(event) {
-//             grids.setAttribute(`style`, `background-color: black`);
-//         });
+//             });
 //     }
-//     });
+// });
 // }
 
-createGrid(16);
+// window.addEventListener(`mouseup`, function(event) {
+//     if (isDrawing === true) {
+//         isDrawing = false;
+//         changeColors(grids, isDrawing);
+//     }
+// });
+
 
 // mousedown begins the sketching
 // mousedown && mouseover continues sketching
 // mouseup stops the sketching
 
-let isDrawing = false;
-const gridsCreated = document.querySelector(`.grids`);
-gridsCreated.addEventListener(`mousedown`, e => {
-    isDrawing = true;
-});
+// let isDrawing = false;
+// let gridIndex;
+// const gridsCreated = document.querySelectorAll(`.grids`);
+// let gridsArray = [];
+// for(let i = gridsCreated.length; i--; gridsArray.unshift(gridsCreated[i]));
 
-gridsCreated.addEventListener(`mouseover`, e => {
-    if (isDrawing === true) {
-        changeColors();
-    }
-});
 
-window.addEventListener(`mouseup`, e => {
-    if (isDrawing === true) {
-        changeColors();
-        isDrawing = false;
-    }
-});
+// gridsCreated.forEach((gridsCreated) => {
+// gridsCreated.addEventListener(`mousedown`, e => {
+//     isDrawing = true;
+// });
+// });
 
-function changeColors() {
-    gridsCreated.setAttribute(`style`, `background-color: black`);
-}
+// gridsCreated.forEach((gridsCreated) => {
+// gridsCreated.addEventListener(`mouseover`, e => {
+//     if (isDrawing === true) {
+//         gridIndex = gridsCreated.index;
+//         console.log(gridIndex);
+//         changeColors(gridIndex);
+//     }
+// });
+// });
+
+// window.addEventListener(`mouseup`, e => {
+//     if (isDrawing === true) {
+//         changeColors(gridIndex);
+//         isDrawing = false;
+//     }
+// });
+
+// function changeColors(index) {
+//     gridsCreated.setAttribute(`style`, `background-color: black`);
+// }
