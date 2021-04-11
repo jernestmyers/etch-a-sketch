8f2e7da5188fe081f2be83347af1281c5c85463f
jernestmyers <inernestmusic@gmail.com>
2021-04-10 23:49:42 -0500

    still struggling to get sketch to start on mousedown and stop on mouseup. spinning wheels.

--- javascript.js
+++ javascript.js
@@ -10,7 +10,6 @@ function createGrid(rows) {
         grids.classList.add('grids');
         gridContainer.appendChild(grids);
         grids.setAttribute('style', `width: ${gridsDimensions}px; height: ${gridsDimensions}px`);
-        changeColors(grids);
     }
     gridTemplate(rows);
 }
@@ -23,21 +22,51 @@ function gridTemplate(rows) {
     gridContainer.setAttribute('style', `grid-template-rows: ${frs}; grid-template-columns: ${frs}`);
 }
 
+
+// let isDrawing = false;
+// function changeColors(grids) {
+//     gridContainer.addEventListener(`mousedown`, function(event) {
+//         isDrawing = true;
+//         window.addEventListener(`mouseup`, function(event) {
+//             isDrawing = false;
+//         });
+//         if (isDrawing === true) {
+//             grids.addEventListener(`mouseover`, function(event) {
+//             grids.setAttribute(`style`, `background-color: black`);
+//         });
+//         } else if (isDrawing === false) {
+//             grids.removeEventListener(`mouseover`, function(event) {
+//             grids.setAttribute(`style`, `background-color: black`);
+//         });
+//     }
+//     });
+// }
+
+createGrid(16);
+
+// mousedown begins the sketching
+// mousedown && mouseover continues sketching
+// mouseup stops the sketching
+
 let isDrawing = false;
-function changeColors(grids) {
-    window.addEventListener(`mouseup`, function(event) {
+const gridsCreated = document.querySelector(`.grids`);
+gridsCreated.addEventListener(`mousedown`, e => {
+    isDrawing = true;
+});
+
+gridsCreated.addEventListener(`mouseover`, e => {
+    if (isDrawing === true) {
+        changeColors();
+    }
+});
+
+window.addEventListener(`mouseup`, e => {
+    if (isDrawing === true) {
+        changeColors();
         isDrawing = false;
-        break
-    });
-    gridContainer.addEventListener(`mousedown`, function(event) {
-        isDrawing = true;
-        if (isDrawing === true) {
-            grids.addEventListener(`mouseover`, function(event) {
-                grids.setAttribute(`style`, `background-color: white`);
-            });
-        }
-        // if (isDrawing === true) {
-           
-        // }
-    });
+    }
+});
+
+function changeColors() {
+    gridsCreated.setAttribute(`style`, `background-color: black`);
 }
