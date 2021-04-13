416a83783019c2b3b7a32e311183cad5c514bb58
jernestmyers <inernestmusic@gmail.com>
2021-04-12 23:20:48 -0500

    added a rainbow pen with a reset function when eraser is turned on or user changes pen color. added resetEraser fxn to when clear grid, rainbow pen, and change grid are clicked. none of the styling issues are resolved yet.

--- javascript.js
+++ javascript.js
@@ -11,6 +11,7 @@ let isDrawing = false;
 colorSelectorDrawing.addEventListener(`change`, () => {
     colorUser = colorSelectorDrawing.value;
     resetEraser();
+    resetRandomizer();
 })
 
 // re-creates grid according to background color choice
@@ -43,12 +44,20 @@ function createGrid(rows) {
         gridContainer.appendChild(grids);
         grids.setAttribute('style', `background-color: ${colorBackground}; width: ${gridsDimensions}px; height: ${gridsDimensions}px`);
         grids.addEventListener('mouseover', () => {
-            if (isDrawing === true) {
+            if (isDrawing === true && randomOn !== true) {
+                grids.setAttribute(`style`, `background-color: ${colorUser}`);
+            } else if (isDrawing === true && randomOn === true) {
+                randomizesRGB();
                 grids.setAttribute(`style`, `background-color: ${colorUser}`);
             }
         });
         grids.addEventListener('mousedown', () => {
+            if (randomOn !== true) {
                 grids.setAttribute(`style`, `background-color: ${colorUser}`);
+            } else if (randomOn === true) {
+                randomizesRGB();
+                grids.setAttribute(`style`, `background-color: ${colorUser}`);
+            }
         });
     }
     gridTemplate(rows);
@@ -72,6 +81,7 @@ createGrid(16);
 const gridSlider = document.querySelector(`#rowsUserChoice`);
 const confirmGridChange = document.querySelector(`#confirmUserGridSize`);
 confirmGridChange.addEventListener('click', () => {
+    resetEraser();
     resetGrid();
     createGrid(gridSlider.value);
 })
@@ -79,6 +89,7 @@ confirmGridChange.addEventListener('click', () => {
 // clears the grid and reloads the currently-selected grid size
 const confirmClear = document.querySelector(`#clearGrid`);
 confirmClear.addEventListener('click', () => {
+    resetEraser();
     resetGrid();
     createGrid(gridSlider.value);
 })
@@ -94,11 +105,10 @@ gridSlider.addEventListener('change', () => {
     inputDiv.appendChild(numbersOfRowsSelected);
 })
 
-
+// used to start sketching on mousedown and stop sketching on mouseup
 gridContainer.addEventListener(`mousedown`, () => {
     isDrawing = true;
 });
-
 window.addEventListener(`mouseup`, () => {
     if (isDrawing === true) {
         isDrawing = false;
@@ -110,17 +120,64 @@ let eraserOn;
 const labelEraser = document.querySelector(`#labelEraser`);
 eraser.addEventListener('click', () => {
         if (eraserOn !== true) {
+            resetRandomizer();
             labelEraser.textContent = `eraser: on`;
             colorUser = colorSelectorBackground.value;
             eraserOn = true;
+            eraser.setAttribute(`style`, `background-color: black`);
+            labelEraser.setAttribute(`style`, `color: white`);
         } else if (eraserOn === true) {
             labelEraser.textContent = `eraser: off`;
             colorUser = colorSelectorDrawing.value;
             eraserOn = false;
+            eraser.setAttribute(`style`, `background-color: white`);
+            labelEraser.setAttribute(`style`, `color: black`);
         } 
     })
 function resetEraser() {
     labelEraser.textContent = `eraser: off`;
     colorUser = colorSelectorDrawing.value;
     eraserOn = false;
+    eraser.setAttribute(`style`, `background-color: white`);
+    labelEraser.setAttribute(`style`, `color: black`);
+}
+
+// creates the color randomizer
+let r;
+let g;
+let b;
+let randomOn;
+const randomColorToggle = document.querySelector(`#randomColor`);
+const labelRandomColor = document.querySelector(`#labelRandomColor`);
+
+randomColorToggle.addEventListener(`click`, () => {
+    if (randomOn !== true) {
+        resetEraser();
+        randomizesRGB();
+        labelRandomColor.textContent = "rainbow pen: on";
+        randomColorToggle.setAttribute(`style`, `background-color: black`);
+        labelRandomColor.setAttribute(`style`, `color: white`);
+    } else if (randomOn === true) {
+        randomOn = false;
+        labelRandomColor.textContent = "rainbow pen: off";
+        randomColorToggle.setAttribute(`style`, `background-color: white`);
+        labelRandomColor.setAttribute(`style`, `color: black`);
+        colorUser = colorSelectorDrawing.value;
+    }
+});
+
+function randomizesRGB() {
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    colorUser = `rgb(${r},${g},${b})`;
+    randomOn = true;
+}
+
+function resetRandomizer() {
+    labelRandomColor.textContent = "rainbow pen: off";
+    colorUser = colorSelectorDrawing.value;
+    randomOn = false;
+    randomColorToggle.setAttribute(`style`, `background-color: white`);
+    labelRandomColor.setAttribute(`style`, `color: black`);
 }
